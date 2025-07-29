11aa218ae8ab94b89e0e48dedfe5c252043bdca9
Your Name <rakibhasanoyoniu@gmail.com>
2025-07-29 11:26:21 +0600

    Project initialized

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,6 @@
+import { Controller } from '@nestjs/common';
+@Controller()
+export class AuthController {
+  // This controller will handle authentication-related routes
+  // You can add methods here to handle login, registration, etc.
+}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,7 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable({})
+export class AuthService {
+  // This service will handle authentication logic
+  // You can add methods here for login, registration, etc.
+}
